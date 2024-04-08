d8850e0ea5cee1a8c60e4ffcf2734d80e1436033
dexian <dxe.tang@gmail.com>
2024-04-08 22:14:09 +0200

    add scan for obj

--- util/types.go
+++ util/types.go
@@ -1,3 +1,17 @@
 package util
 
+import "encoding/json"
+
 type Obj map[string]any
+
+func (o *Obj) Scan(value interface{}) error {
+	if value == nil {
+		*o = Obj{}
+		return nil
+	}
+	bytes, ok := value.([]byte)
+	if !ok {
+		return ErrNotBytes
+	}
+	return json.Unmarshal(bytes, o)
+}
