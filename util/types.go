97b6d46d65c8c214e3938396e3f5f5b70b8cfcf6
dexian <dxe.tang@gmail.com>
2024-04-09 10:02:37 +0200

    add Objs

--- util/types.go
+++ util/types.go
@@ -3,6 +3,19 @@ package util
 import "encoding/json"
 
 type Obj map[string]any
+type Objs []Obj
+
+func (o *Objs) Scan(value interface{}) error {
+	if value == nil {
+		*o = Objs{}
+		return nil
+	}
+	bytes, ok := value.([]byte)
+	if !ok {
+		return ErrNotBytes
+	}
+	return json.Unmarshal(bytes, o)
+}
 
 func (o *Obj) Scan(value interface{}) error {
 	if value == nil {
