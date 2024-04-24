e5d165aa61917a27f95f8a0a23a3daaf83f07a84
dexian <dxe.tang@gmail.com>
2024-04-24 17:08:05 +0200

    add NullFloat64

---
+++ util/type_test.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNullFloat64(t *testing.T) {
+	db := NewTestDB("")
+
+	var f NullFloat64
+	err := db.QueryRow("SELECT null").Scan(&f)
+	assert.Nil(t, err)
+	assert.True(t, f.Null)
+	assert.Zero(t, f.V)
+
+	err = db.QueryRow("SELECT 1").Scan(&f)
+	assert.Nil(t, err)
+	assert.False(t, f.Null)
+	assert.Equal(t, f.V, 1.)
+
+	b, _ := json.Marshal(f)
+	assert.Equal(t, string(b), "1")
+}
--- util/types.go
+++ util/types.go
@@ -1,9 +1,39 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
 
 type Obj map[string]any
 type Objs []Obj
+type NullFloat64 struct {
+	Null bool
+	V    float64
+}
+
+func (o *NullFloat64) Scan(v any) error {
+	*o = NullFloat64{}
+	if v == nil {
+		o.Null = true
+		return nil
+	}
+
+	f, err := strconv.ParseFloat(fmt.Sprint(v), 64)
+	if err != nil {
+		return err
+	}
+	(*o).V = f
+	return nil
+}
+
+func (o NullFloat64) MarshalJSON() ([]byte, error) {
+	if o.Null {
+		return []byte("null"), nil
+	}
+	return json.Marshal(o.V)
+}
 
 func (o *Objs) Scan(value interface{}) error {
 	if value == nil {
